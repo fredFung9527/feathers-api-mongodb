051de8abfe5fa8aacf28f530c3c070653db0d89c
Fred <ylfungfred@gmail.com>
2021-05-12 16:05:48 +0800

    removeOriginalMongo

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,14 +1,7 @@
-import { MongoClient } from 'mongodb';
 import { Application } from './declarations';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 export default function (app: Application): void {
   const connection = app.get('mongodb');
   mongoose.connect(connection);
-
-/*   const database = connection.substr(connection.lastIndexOf('/') + 1);
-  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
-    .then(client => client.db(database));
-
-  app.set('mongoClient', mongoClient); */
 }
--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -12,10 +12,6 @@ declare module '../../declarations' {
 }
 
 export default function (app: Application): void {
-  const options = {
-    paginate: app.get('paginate')
-  };
-
   // Initialize our service with any options it requires
   app.use('/users', Model);
 
