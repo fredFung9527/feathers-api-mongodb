75234620e3e008cdaff772244c4836f238c538b2
Fred <ylfungfred@gmail.com>
2021-05-13 14:55:47 +0800

    bulk import and configure

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
 import { Application } from '../declarations';
-import users from './users/users.service';
+const requireContext = require('node-require-context')
+import _ from "lodash"
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
-  app.configure(users);
+  const services = requireContext("./", true, /\.service\.ts$/);
+  _.forEach(services.keys(), moduleId => {
+    app.configure(services(moduleId).default);
+  });
 }
