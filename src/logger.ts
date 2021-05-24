fa073b6c81b837339637d7cf69169ce80e5c7bdd
Fred <ylfungfred@gmail.com>
2021-05-24 15:26:03 +0800

    logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,22 @@
 import { createLogger, format, transports } from 'winston';
 
 // Configure the Winston logger. For the complete documentation see https://github.com/winstonjs/winston
+const myFormat = format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
+
 const logger = createLogger({
   // To see more detailed errors, change this to 'debug'
   level: 'info',
   format: format.combine(
     format.splat(),
-    format.simple()
+    format.simple(),
+    format.timestamp(),
+    myFormat
   ),
   transports: [
-    new transports.Console()
+    new transports.Console(),
+    new transports.File({ filename: 'logger.log' }),
   ],
 });
 
