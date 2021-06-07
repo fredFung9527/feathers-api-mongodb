c92fa02b2fe32a9c790ad778f5fce20f7a049d0d
Fred <ylfungfred@gmail.com>
2021-06-07 14:58:44 +0800

    finishUpload

--- src/services/attachments/service.ts
+++ src/services/attachments/service.ts
@@ -4,6 +4,7 @@ import hooks from './hooks';
 import createServie from "feathers-mongoose";
 const { authenticate } = require('@feathersjs/express');
 import multer from 'multer';
+import fs from 'fs';
 
 declare module '@declarations' {
   interface ServiceTypes {
@@ -19,23 +20,42 @@ declare module '@declarations' {
 
 export default function (app: Application): void {
   const storage = multer.diskStorage({
-    destination: (_req, _file, cb) => cb(null, 'public/attachments'),
-    filename: (_req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
+    destination: (req: any, file: Express.Multer.File, cb) => {
+      const userID: string = req.user._id;
+      const dir = `public/attachments/${userID ? userID : 'no-user'}`;
+      if (fs.existsSync(dir)) {
+        cb(null, 'public/attachments/' + req.user._id)
+      } else {
+        fs.mkdir(dir, error => cb(error, dir));
+      }
+    },
+    filename: (req, file: Express.Multer.File, cb) => {
+      cb(null, `${req.body?.tag || 'common'}-${Date.now()}-${file.originalname}`);
+    },
   });
   const upload = multer({
     storage,
     limits: {
       fileSize: 1e+7,
+    },
+    fileFilter: (req, file: Express.Multer.File, cb) => {
+      if (file.mimetype.includes('image')) {
+        cb(null, true);
+      } else {
+        cb(null, false);
+      }
     }
   });
 
   app.use('/attachments',
     authenticate('jwt'),
     upload.single('file'), 
-    (req: any, _res, next) => {
+    (req, res, next) => {
+      if (!req.file) throw new Error('No file attached / Invalid file type');
       if (req.method === 'POST') {
         req.body = {
           description: req.body.description,
+          tag: req.body.tag,
           name: req.file.originalname,
           path: req.file.path,
         };
